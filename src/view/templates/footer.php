45af37d6c31779f5cc9872b6f8279a04cb3d441b
DaniilGrachev <prodanypropb@gmail.com>
2024-07-28 21:48:01 +0300

    created single file with footer to simplify making other pages

---
+++ src/view/templates/footer.php
@@ -0,0 +1,25 @@
+<!-- footer.php -->
+<footer>
+    <div class="blocks container">
+        <div>
+            <span class="logo">MixologyHub</span>
+            <p>Website for diving in a good mood!</p>
+        </div>
+        <div>
+            <h4>About us</h4>
+            <ul>
+                <li>Owner</li>
+                <li>GitHub</li>
+                <li>Contact Us</li>
+            </ul>
+        </div>
+        <div>
+            <h4>Contact us</h4>
+            <p><a href="https://t.me/danyame0_0" target="_blank">Telegram</a></p>
+            <p><a href="mailto:prodanypropb@gmail.com?cc=Job Offer&subject=Job%20offer%20to%20Daniil%20Grachev&body=Hello%20Daniil%20Grachev%2C%20...">Email</a></p>
+            <p><a href="https://github.com/DaniilGrachev" target="_blank">GitHub</a></p>
+        </div>
+    </div>
+    <hr>
+    <p>Copyright © Grachev Daniil 2024</p>
+</footer>
