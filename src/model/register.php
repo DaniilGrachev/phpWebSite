b6477a9e1ad3a7a3689961728b2f166808c0e750
DaniilGrachev <prodanypropb@gmail.com>
2024-08-01 00:15:29 +0300

    created functions for validating data from register page

--- src/model/register.php
+++ src/model/register.php
@@ -1,1 +1,35 @@
 <?php
+
+require_once __DIR__ . '/../controller/helpers.php';
+
+$name = $_POST['name'] ?? null;
+$email = $_POST['email'] ?? null;
+$password = $_POST['password'] ?? null;
+$conf_password = $_POST['conf_password'] ?? null;
+
+addOldValue('name', $name);
+addOldValue('email', $email);
+
+//Validation
+
+if (empty($name)){
+    addValidationError('name', 'Incorrect Name');
+}
+
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+    addValidationError('email', 'Incorrect Email');
+}
+
+if (!empty($password))
+{
+    addValidationError('password', 'Password cannot be empty');
+}
+
+if ($password === $conf_password)
+{
+    addValidationError('conf_password', "Passwords are different");
+}
+
+if (!empty($_SESSION['validation'])) {
+    redirect('/phpWebSite/src/view/templates/reg.php');
+}
